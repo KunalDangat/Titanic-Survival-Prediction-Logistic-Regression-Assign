006919eeb6c0033f62863230e976e681d4c14663
KunalDangat <142024037+KunalDangat@users.noreply.github.com>
2024-06-04 00:40:53 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1qUyHt5YAIBYpfPDoM-QTHgSoVF5CfZPn
 """
 
-pip install streamlit
+#pip install streamlit
 
 import streamlit as st
 import pandas as pd
